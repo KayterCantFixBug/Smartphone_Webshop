c19980ca9bc0c56bb3c69b32b9cdf948b7a465e0
panda <vantrungbusiness@gmail.com>
2023-12-03 01:20:46 +0700

    UpdateOrder_OrderDetail

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -7,25 +7,69 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "order_id")
-    private  int id;
+    @Column(name = "order_id")
+    private int id;
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public enum Status {
+        DELIVERING, COMPLETE
+    }
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
-    @Column (name = "estimate_date")
-    private Date estimateDate;
-    @Column (name = "address")
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_detail_id")
+    private List<OrderDetail> orderDetails;
+    @Column(name = "date")
+    private String Date;
+    @Column(name = "address")
     private String address;
-    @Column (name = "status")
-    private int status;
-    @Column (name = "note")
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "status")
+    private Status status;
+    @Column(name = "note")
     private String note;
+
+    public Order() {
+
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
     public int getId() {
         return id;
@@ -33,38 +77,6 @@ public class Order {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public Date getEstimateDate() {
-        return estimateDate;
-    }
-
-    public void setEstimateDate(Date estimateDate) {
-        this.estimateDate = estimateDate;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
     }
 
     public User getUser() {
@@ -76,4 +88,73 @@ public class Order {
     }
 
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Order(String date) {
+        Date = date;
+    }
+
+    public Order(int id, String address, String phone, String note) {
+        this.id = id;
+        this.address = address;
+        this.phone = phone;
+        this.note = note;
+    }
+
+    public void addOrderDetail(OrderDetail orderDetail) {
+        int code = orderDetail.getProduct().getId();
+        int quantity = orderDetail.getQuantity();
+        for (int i = 0; i < this.orderDetails.size(); i++) {
+            OrderDetail orderDetail_temp = orderDetails.get(i);
+            if (orderDetail_temp.getProduct().getId() == code) {
+                orderDetail_temp.setQuantity(quantity);
+                return;
+            }
+        }
+        orderDetails.add(orderDetail);
+    }
+    public int getQuantityOrderDetail (OrderDetail orderDetail){
+        int product_id = orderDetail.getProduct().getId();
+        for (int i = 0; i < orderDetails.size(); i++) {
+            OrderDetail orderDetail1_temp = orderDetails.get(i);
+            if (orderDetail1_temp.getProduct().getId() == product_id) {
+                return orderDetail1_temp.getQuantity();
+            }
+        }
+        return -1;
+    }
+
+    public void removeOrderDetail(OrderDetail orderDetail) {
+        int code = orderDetail.getProduct().getId();
+        for (int i = 0; i < orderDetails.size(); i++) {
+            OrderDetail orderDetail_temp = orderDetails.get(i);
+            if (orderDetail_temp.getProduct().getId()==code) {
+                orderDetails.remove(i);
+                return;
+            }
+        }
+    }
+
+    public Order(User user, String date) {
+        this.user = user;
+        Date = date;
+    }
 }
+
+
+
--- src/main/java/model/OrderDetail.java
+++ src/main/java/model/OrderDetail.java
@@ -2,21 +2,27 @@ package model;
 
 import jakarta.persistence.*;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 @Entity
-@Table(name = "orderdetail")
+@Table(name = "orderdetails")
 public class OrderDetail {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name = "order_detail_id")
     private int id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
+
+    @Column(name = "quantity")
+    private int quantity;
 
     public Product getProduct() {
         return product;
@@ -32,5 +38,47 @@ public class OrderDetail {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        double total = product.getPrice() * quantity;
+        return total;
+    }
+    public String getTotalCurrencyFormat() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.US);
+        return currency.format(this.getTotal());
+    }
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public OrderDetail(Product product) {
+        this.product = product;
+    }
+
+    public OrderDetail(int id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
     }
 }
